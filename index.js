f1c5800ef3b8e5ed29ff99f6d285c4a42a16564f
Patrick Kalkman <patrick@simpletechture.nl>
2018-11-04 21:33:38 +0100

    Setup environment, started to work on user service

--- index.js
+++ index.js
@@ -1,62 +1,20 @@
+/*
+* Primary file for the API
+*
+*/
+
 'use strict';
-/* Main entry point of the app */
 
-// Dependencies
-const http = require('http');
-const utils = require('util');
-const debug = utils.debuglog('index');
-const helpers = require('./lib/helpers');
-const routes = require('./lib/routes');
-const constants = require('./lib/constants');
+//Dependencies
+var server = require('./lib/server');
 
-const server = http.createServer((req, res) => {
+var app = {};
 
-  var data = {};
-  data.httpMethod = req.method.toLowerCase();
-  data.trimmedPath = helpers.getTrimmedPath(req.url);
-  data.payload = '';
-  data.headers = req.headers;
+app.init = function() {
+  server.init();
+};
 
-  routes.handle(data, function(statusCode, responsePayload) {
+// Execute the function
+app.init();
 
-    console.log(statusCode);
-    responsePayload = typeof (payload) === 'object' ? responsePayload : {};
-
-    const responsePayloadString = JSON.stringify(responsePayload);
-    res.setHeader('content-type', 'application/json');
-    res.writeHead(statusCode);
-    res.end(responsePayloadString);
-    
-    if (statusCode === 200) {
-      debug('\x1b[32m%s\x1b[0m', data.httpMethod.toUpperCase() + ' /' + data.trimmedPath +' ' + statusCode);
-    } else { 
-      debug('\x1b[31m%s\x1b[0m', data.httpMethod.toUpperCase() + ' /' + data.trimmedPath +' ' + statusCode);
-    }
-    // log the requested path
-    debug('Returning ', statusCode, responsePayloadString);
-  });
-
-});
-
-const usersPath = 'users';
-
-routes.add(usersPath, constants.HTTP_METHOD_POST, function(data, callback) {
-  
-  var response = { 'message' : 'Hello from users POST'};
-
-  callback(constants.HTTP_STATUS_OK, response);
-
-});
-
-routes.add(usersPath, constants.HTTP_METHOD_GET, function(data, callback) {
-  var response = { 'message' : 'Hello from users GET'};
-
-  callback(constants.HTTP_STATUS_OK, response);
-
-});
-
-server.listen(3000, function() {
-  console.log('http server is started and listening on port 3000');
-});
-
-
+module.exports = app;
